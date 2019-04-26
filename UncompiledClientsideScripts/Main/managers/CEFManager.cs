b039e4c12c89458748495b1c4792f94aee3ee1a7
CrosRoad95 <sebajura1234@gmail.com>
2019-04-26 16:39:56 +0200

    Wyświetlenie prostego "hello world" w cefie

---
+++ UncompiledClientsideScripts/Main/managers/CEFManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RAGE;
+using CEF = RAGE.Ui.HtmlWindow;
+
+namespace Manager
+{
+    public class CCEFManager
+    {
+        CEF browser;
+        public CCEFManager()
+        {
+            browser = new CEF("package://html/index.html");
+
+            browser.Active = true;
+        }
+    }
+}
--- UncompiledClientsideScripts/Main/managers/Managers.cs
+++ UncompiledClientsideScripts/Main/managers/Managers.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 using Manager;
+
 namespace Manager
 {
     public class CManagers
     {
         public CRPCManager rpc;
+        public CCEFManager cef;
         public CManagers()
         {
             rpc = new CRPCManager();
+            cef = new CCEFManager();
         }
     }
 }
